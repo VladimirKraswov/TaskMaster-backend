557388d4ad6fc186279415be6d1922e3637439e3
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2026-02-27 15:47:20 +0300

    move task and column

---
+++ src/schemas/columns.ts
@@ -0,0 +1,136 @@
+
+export const CreateColumnSchema = {
+      tags: ['tasks'],
+      summary: 'создать колонку в доске',
+      security: [{ bearerAuth: [] }],
+      params: {
+        type: 'object',
+        required: ['boardId'],
+        properties: {
+          boardId: { type: 'integer' },
+        },
+      },
+      body: {
+        type: 'object',
+        required: ['title'],
+        properties: {
+          title: { type: 'string', minLength: 1, maxLength: 255 },
+        },
+      },
+      response: {
+        201: {
+          type: 'object',
+          properties: {
+            id: { type: 'integer' },
+            title: { type: 'string' },
+            display_order: { type: 'string' },
+            board_id: { type: 'integer' },
+          },
+        },
+        400: {
+          type: 'object',
+          properties: {
+            error: { type: 'string' },
+          },
+        },
+      },
+    }
+
+  export const MoveolumnSchema = {
+      tags: ['tasks'],
+      summary: 'переместить колонку в доске',
+      security: [{ bearerAuth: [] }],
+      params: {
+        type: 'object',
+        required: ['boardId', 'columnId'],
+        properties: {
+          boardId: { type: 'integer' },
+          columnId: { type: 'integer' },
+        },
+      },
+      body: {
+        type: 'object',
+        properties: {
+          targetColumnId: { type: 'string'},
+          placement: { type: 'string', enum:['before', 'after','start', 'end'] },
+        },
+      },
+      response: {
+        201: {
+          type: 'object',
+          properties: {
+            id: { type: 'integer' },
+            title: { type: 'string' },
+            board_id: { type: 'integer' },
+            display_order: { type: 'string' },
+          },
+        },
+        400: {
+          type: 'object',
+          properties: {
+            error: { type: 'string' },
+          },
+        },
+      },
+    }
+
+export const editColumnSchema = {
+    tags: ['tasks'],
+    summary: 'обновить колонку',
+    security: [{ bearerAuth: [] }],
+    params: {
+      type: 'object',
+      required: ['boardId', 'columnId'],
+      properties: {
+        boardId: { type: 'integer' },
+        columnId: { type: 'integer' }
+      }
+    },
+    body: {
+      type: 'object',
+      required: ['title', 'display_order'],
+      properties: {
+        title: { type: 'string', minLength: 1, maxLength: 255 },
+      }
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          id: { type: 'integer' },
+          title: { type: 'string' },
+          display_order: { type: 'string' },
+          board_id: { type: 'integer' }
+        }
+      },
+      404: {
+        type: 'object',
+        properties: { error: { type: 'string' } }
+      }
+    }
+  }
+
+
+export const deleteColumnSchema = {
+    tags: ['tasks'],
+    summary: 'удалить колонку',
+    security: [{ bearerAuth: [] }],
+    params: {
+      type: 'object',
+      required: ['boardId', 'columnId'],
+      properties: {
+        boardId: { type: 'integer' },
+        columnId: { type: 'integer' }
+      }
+    },
+    response: {
+      204: {
+        type: 'null',
+        description: 'No content'
+      },
+      404: {
+        type: 'object',
+        properties: { error: { type: 'string' } }
+      }
+    }
+}
